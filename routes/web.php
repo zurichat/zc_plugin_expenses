ce6b64bf1b4a134f1da53437b039f2b6bd664426
pizzii <dono4executive@gmail.com>
2021-09-04 23:31:18 +0100

    add styling to tabelhead

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/expense_list', function () {
+    return view('expense_list');
+});
