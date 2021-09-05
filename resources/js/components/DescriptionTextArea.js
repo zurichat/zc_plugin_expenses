418356c9841322d4084a05dd96cba8ef411d9381
Celestine <47569127+Styne001@users.noreply.github.com>
2021-09-05 13:31:27 +0100

    DescriptionText Area in Create new expense list form
    Created the description text area component in Create new expense list form

---
+++ resources/js/components/DescriptionTextArea.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+//@Celestine001 issue #132
+export const DescriptionTextArea = () => {
+    return (
+        <div>
+            <textarea></textarea>
+        </div>
+    )
+}
