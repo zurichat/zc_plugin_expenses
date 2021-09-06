ee103b19446fbf9b71fbeff16266ad3fd2de7c4d
Chekwube Ezechi <zechirayjr@yahoo.com>
2021-09-06 09:16:03 +0100

    updated file

---
+++ resources/js/components/create/FormCancelElement.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function FormCancleElement(props) {
+    return (
+        <div className="form-group col-md-3">
+            <a href="/home"className="btn btn-outline-success">Cancle</a>
+        </div>
+    )
+}
