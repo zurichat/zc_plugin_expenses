a0b46ab05c9399de52bdd486729a92c0833cbe1b
airondev <airondev@gmail.com>
2021-09-13 15:23:35 +0100

    zuri core write interface init

--- app/Helpers/ReadWrite.php
+++ app/Helpers/ReadWrite.php
@@ -17,6 +17,7 @@ class ReadWrite
 	protected $object_id = '';
 	protected $filter = '';
 	protected $payload = '';
+    protected static $apiBase = 'https://api.zuri.chat/data/write';
 
 
 	public static function write($payload, $filter = null, $object_id = null )
@@ -24,7 +25,16 @@ class ReadWrite
 
         // send data to zuri core here for write operation
         //  POST: /data/write/{plugin_id}/{collection_name}/{organization_id}/?_id=""
-        return $payload;
+        $body = [
+            'plugin_id' => '613ba9de41f5856617552f51',
+            'organization_id' => '6133c5a68006324323416896',
+            'collection_name' => 'Expense',
+            'payload' => $payload
+        ];
+        $header = [];
+        $method = "POST";
+        $res = static::guzzleMethod($body, $header, $method);
+        return $res;
     }
 
 
@@ -38,5 +48,28 @@ class ReadWrite
     public static function delete($filter = null, $object_id = null )
     {
         
+    }
+
+
+    public static function guzzleMethod ($body, $header, $method){
+        try {
+
+            $url = static::$apiBase;
+            $client = new \GuzzleHttp\Client();
+            $res = $client->request('POST', "{$url}", [
+                'body' => json_encode($body),
+                'headers' => $header
+            ]);
+
+            $response = (json_decode($res->getBody()->getContents()));
+            if( $response ){
+               return $response;
+            } else{
+                return;
+            }
+            
+        } catch (Exception $e) {
+            return $e;
+        }
     }
 }
