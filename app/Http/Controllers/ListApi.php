b38377e834328a42c3bda07099f28b893eaa55ff
kelani aliyu <kelanialiyu@gmail.com>
2021-09-14 02:33:54 +0100

    making list api write to zuri core

--- app/Http/Controllers/ListApi.php
+++ app/Http/Controllers/ListApi.php
@@ -15,7 +15,23 @@ class ListApi extends Controller
      */
     public function index(Request $request)
     {
-        return $this->write();
+        $data["url"] = "https://api.zuri.chat/data/write";
+        $data["plugin_id"] = "613ba9de41f5856617552f51";
+        $data["organization_id"] = "6133c5a68006324323416896";
+        $data["collection_name"] = "expenses_rooms_collection";
+        $data["bulk_write"]=false;
+        $data["object_id"]="";
+        $data["filter"]=null;
+        $data["payload"] = [
+                "room_name" => "Accountants",
+                "creator_id" => "613d3e65e4010959c8dc0c11",
+                "visibility" => "public",
+                "users_id" => ["613d3e65e4010959c8dc0c11"],
+                "creation_date" => "date()",
+                "plugin_id" => "613ba9de41f5856617552f51",
+                "organisation_id" => "6133c5a68006324323416896"
+        ];
+        return $this->write("expenses_rooms_collection",$data);
     }
     
     /**
@@ -68,26 +84,37 @@ class ListApi extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function write($data=null, $method=null, $url=null,$filter=null)
+    public function write($collection_name,$data=null,$url = "https://api.zuri.chat/data/write")
     {
         
-        //$response = Http::post("https://api.zuri.chat/data/write", [
-         $response = [   "plugin_id" => "613ba9de41f5856617552f51",
-            "organization_id" => "6133c5a68006324323416896",
-            "collection_name" => "expenses_rooms_collection",
-            "bulk_write" => false,
-            "object_id" => "",
-            "filter" =>null,
-            "payload" =>[
-                "room_name" => "Accountants",
-                "creator_id" => "613d3e65e4010959c8dc0c11",
-                "visibility" => "public",
-                "users_id" => ["613d3e65e4010959c8dc0c11"],
-                "creation_date" => "date()",
-                "plugin_id" => "613ba9de41f5856617552f51",
-                "organisation_id" => "6133c5a68006324323416896"
-            ]
-            ];//);
+        $response = Http::post($url = "https://api.zuri.chat/data/write",[
+            "plugin_id" => $data["plugin_id"],
+            "organization_id" => $data["organization_id"],
+            "collection_name" => $collection_name,
+            "bulk_write" => $data["bulk_write"],
+            "object_id" => $data["object_id"],
+            "filter" =>$data["filter"],
+            "payload" =>$data["payload"]
+            ]);
         return $response;
     }
 }
+
+// $response = Http::post("https://api.zuri.chat/data/write", [
+//             "plugin_id" => "613ba9de41f5856617552f51",
+//             "organization_id" => "6133c5a68006324323416896",
+//             "collection_name" => "expenses_rooms_collection",
+//             "bulk_write" => false,
+//             "object_id" => "",
+//             "filter" =>null,
+//             "payload" =>[
+//                 "room_name" => "Accountants",
+//                 "creator_id" => "613d3e65e4010959c8dc0c11",
+//                 "visibility" => "public",
+//                 "users_id" => ["613d3e65e4010959c8dc0c11"],
+//                 "creation_date" => "date()",
+//                 "plugin_id" => "613ba9de41f5856617552f51",
+//                 "organisation_id" => "6133c5a68006324323416896"
+//             ]
+//             ]);
+//         return $response;
