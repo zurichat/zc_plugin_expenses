7d12d71f61ff959493cfe0ab65a98af7fd123e27
kelani aliyu <kelanialiyu@gmail.com>
2021-09-15 12:51:28 +0100

    adding required features to list api

--- app/Http/Controllers/ListApi.php
+++ app/Http/Controllers/ListApi.php
@@ -15,23 +15,16 @@ class ListApi extends Controller
      */
     public function index(Request $request)
     {
-        $data["url"] = "https://api.zuri.chat/data/write";
-        $data["plugin_id"] = "613ba9de41f5856617552f51";
-        $data["organization_id"] = "6133c5a68006324323416896";
-        $data["collection_name"] = "expenses_rooms_collection";
-        $data["bulk_write"]=false;
-        $data["object_id"]="";
-        $data["filter"]=null;
-        $data["payload"] = [
-                "room_name" => "Accountants",
-                "creator_id" => "613d3e65e4010959c8dc0c11",
-                "visibility" => "public",
-                "users_id" => ["613d3e65e4010959c8dc0c11"],
-                "creation_date" => "date()",
-                "plugin_id" => "613ba9de41f5856617552f51",
-                "organisation_id" => "6133c5a68006324323416896"
-        ];
-        return $this->write("expenses_rooms_collection",$data);
+        $request->validate([
+            "plugin_id" => "required",
+            "organization_id" => "required",
+            "room_id" => "required"
+        ]);
+
+        $data["plugin_id"] = $request->plugin_id;
+        $data["organization_id"] = $request->organization_id;
+        $query = ["room_id" => $request->room_id];
+        return $this->read("expenses_list_collection",$data,$query);
     }
     
     /**
@@ -42,8 +35,39 @@ class ListApi extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            "plugin_id" => "required",
+            "organization_id" => "required",
+            "room_id" => "required",
+            "author_id"=>"required",
+            "title" =>"required"
+        ]);
+        $total = 0;
+        foreach ($request->items as $item ) {
+           $total+=$item["unit_price"] * $item["quantity"];
+        }
+        // return $total;
+        $data["url"] = "https://api.zuri.chat/data/write";
+        $data["plugin_id"] = $request->plugin_id;
+        $data["organization_id"] = $request->organization_id;
+        $data["collection_name"] = "expenses_list_collection";
+        $data["bulk_write"]=false;
+        $data["object_id"]="";
+        $data["filter"] =json_decode("{}");
+        $data["payload"] = [
+                "room_id" => $request->room_id,
+                "author_id" => $request->author_id,
+                "tittle" => $request->title,
+                "items" => $request->items,
+                "total" =>$total,
+                "description" => $request->description,
+                "status" => "pending",
+                "admin_comment" =>"",
+                "creation_date" => time()
+        ];
+        // return $data;
+        return $this->write("expenses_list_collection",$data);
+     }
 
     /**
      * Display the specified resource.
@@ -51,9 +75,21 @@ class ListApi extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id,Request $request)
     {
-        //
+        $request->validate([
+            "plugin_id" => "required",
+            "organization_id" => "required",
+            "room_id" => "required"
+        ]);
+
+        $data["plugin_id"] = $request->plugin_id;
+        $data["organization_id"] = $request->organization_id;
+        $query = [
+            "room_id" => $request->room_id,
+            "_id" =>$id
+        ];
+        return $this->read("expenses_list_collection",$data,$query);
     }
 
     /**
@@ -65,7 +101,20 @@ class ListApi extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            "plugin_id" => "required",
+            "organization_id" => "required",
+            "room_id" => "required",
+            "list_status" =>"required"
+        ]);
+
+        $data["plugin_id"] = $request->plugin_id;
+        $data["organization_id"] = $request->organization_id;
+        $query = [
+            "room_id" => $request->room_id,
+            "_id" =>$id
+        ];
+        return $this->read("expenses_list_collection",$data,$query);
     }
 
     /**
@@ -98,7 +147,20 @@ class ListApi extends Controller
             ]);
         return $response;
     }
+     /**
+     * Writes Data to an zuricore api.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function read($collection_name, $data = null, $query = null,$url = "https://api.zuri.chat/data/read")
+    {
+        
+        $response = Http::get($url = "$url/{$data['plugin_id']}/{$collection_name}/{$data['organization_id']}",$query);
+        return $response->json();
+        // $message = ($response->successful())?"Expense list created successfully":$response->message();
+    }
 }
+
 
 // $response = Http::post("https://api.zuri.chat/data/write", [
 //             "plugin_id" => "613ba9de41f5856617552f51",
